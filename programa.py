284e913a336836cc9bae4295b7588c4550982004
lucasbsantana2007 <lucassantana220903@gmail.com>
2025-10-23 19:46:44 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,13 +1,34 @@
-from funcoes import define_posicoes, preenche_frota, posicao_valida
+from funcoes import *
 
 frota = {}
-tipos_de_navios = { 
-    "porta-aviões": (4,1),
-    "navio-tanque": (3,2),
-    "contratorpedeiro": (2, 3),
-    "submarino": (1, 4)
-}
-for nome, (tamanho, quantidade) in tipos_de_navios.items():
-    for i in range(quantidade):
+
+navios = [
+    ("porta-aviões", 4, 1),
+    ("navio-tanque", 3, 2),
+    ("contratorpedeiro", 2, 3),
+    ("submarino", 1, 4)
+]
+
+for nome, tamanho, quantidade in navios:
+    for _ in range(quantidade):
         print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
-        posicao_certa = False
+        while True:
+            linha = int(input("Linha: "))
+            coluna = int(input("Coluna: "))
+
+            # Submarino não precisa perguntar orientação
+            if nome == "submarino":
+                orientacao = "vertical"  # tanto faz para tamanho 1
+            else:
+                opc = int(input("[1] Vertical [2] Horizontal >"))
+                while opc not in (1, 2):
+                    opc = int(input("[1] Vertical [2] Horizontal >"))
+                orientacao = "vertical" if opc == 1 else "horizontal"
+
+            if posicao_valida(frota, linha, coluna, orientacao, tamanho):
+                frota = preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+                break
+            else:
+                print("Esta posição não está válida!")
+
+print(frota)
