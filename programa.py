acb8d017fb30f3f69858d94e8772461e3301361e
jpmarrar <Jpmarrar@uol.com.br>
2025-10-23 15:20:08 -0300

    Refactor print statements and format frota output

--- programa.py
+++ programa.py
@@ -1,15 +1,12 @@
 from funcoes import define_posicoes, preenche_frota, posicao_valida
 
-frota = {"porta-aviões": [],"navio-tanque": [],"contratorpedeiro": [],"submarino": []}
-
+frota = {"porta-aviões": [], "navio-tanque": [], "contratorpedeiro": [], "submarino": []}
 
 navios_info = {"porta-aviões": {"tamanho": 4, "quantidade": 1},"navio-tanque": {"tamanho": 3, "quantidade": 2},"contratorpedeiro": {"tamanho": 2, "quantidade": 3},"submarino": {"tamanho": 1, "quantidade": 4}}
 
-
 for nome_navio, info in navios_info.items():
-    print(f"Insira as informações referentes ao navio {nome_navio} que possui tamanho {info['tamanho']}", end=" ")
-
     for i in range(info["quantidade"]):
+        print(f"Insira as informações referentes ao navio {nome_navio} que possui tamanho {info['tamanho']}", end=" ")
         posicao_valida_flag = False
 
         while not posicao_valida_flag:
@@ -20,13 +17,12 @@ for nome_navio, info in navios_info.items():
                 orientacao_input = int(input("[1] Vertical [2] Horizontal >"))
                 orientacao = "vertical" if orientacao_input == 1 else "horizontal"
             else:
-                orientacao = "vertical" 
+                orientacao = "vertical"
 
-    
             if posicao_valida(frota, linha, coluna, orientacao, info["tamanho"]):
                 frota = preenche_frota(frota, nome_navio, linha, coluna, orientacao, info["tamanho"])
                 posicao_valida_flag = True
             else:
                 print("Esta posição não está válida!", end=" ")
 
-print(frota)
+print(f"{frota}", end="")
