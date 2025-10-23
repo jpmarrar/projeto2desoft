005139109cfb8f59de415beab22555c2f259c18f
jpmarrar <Jpmarrar@uol.com.br>
2025-10-23 00:02:50 -0300

    Implement fleet positioning in funcoes.py
    Add function to position fleet on the board

--- funcoes.py
+++ funcoes.py
@@ -21,3 +21,16 @@ def faz_jogada(tabuleiro, linha, coluna):
     else:
         tabuleiro[linha][coluna] = '-'
     return tabuleiro
+    
+def posiciona_frota(frota):
+    tabuleiro = []
+    for i in range(10):
+        linha = [0] * 10
+        tabuleiro.append(linha)
+    for nome_navio in frota:
+        for navio in frota[nome_navio]: 
+            for posicao in navio:
+                linha = posicao[0]
+                coluna = posicao[1]
+                tabuleiro[linha][coluna] = 1
+    return tabuleiro
