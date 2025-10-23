2e4804f7b7672c37f3daa7be0113d8d6ee28a075
jpmarrar <Jpmarrar@uol.com.br>
2025-10-22 23:55:24 -0300

    Add faz_jogada function to update game board

--- funcoes.py
+++ funcoes.py
@@ -14,3 +14,10 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     else:
         frota[nome_navio] = [posicoes]
     return frota
+    
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    else:
+        tabuleiro[linha][coluna] = '-'
+    return tabuleiro
