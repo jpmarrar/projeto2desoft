07eb6eed525e289dc20c502e9c0ba670606d3d37
lucasbsantana2007 <lucassantana220903@gmail.com>
2025-10-23 17:56:54 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,30 +1,42 @@
-from funcoes import define_posicoes, preenche_frota, posicao_valida
+frota = {
+    "porta-aviões": [],
+    "navio-tanque": [],
+    "contratorpedeiro": [],
+    "submarino": []
+}
 
-frota = {"porta-aviões": [], "navio-tanque": [], "contratorpedeiro": [], "submarino": []}
+especificacao = [
+    ("porta-aviões",    4, 1),
+    ("navio-tanque",    3, 2),
+    ("contratorpedeiro",2, 3),
+    ("submarino",       1, 4),
+]
 
-navios_info = {"porta-aviões": {"tamanho": 4, "quantidade": 1},"navio-tanque": {"tamanho": 3, "quantidade": 2},"contratorpedeiro": {"tamanho": 2, "quantidade": 3},"submarino": {"tamanho": 1, "quantidade": 4}}
+def ler_int(msg):
+    while True:
+        try:
+            return int(input(msg))
+        except ValueError:
+            pass
 
-for nome_navio, info in navios_info.items():
-    for i in range(info["quantidade"]):
-        posicao_valida_flag = False
+for nome, tamanho, qtd in especificacao:
+    for _ in range(qtd):
+        while True:
+            print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
+            linha  = ler_int("Linha: ")
+            coluna = ler_int("Coluna: ")
 
-        while not posicao_valida_flag:
-            print(f"Insira as informações referentes ao navio {nome_navio} que possui tamanho {info['tamanho']}", end=" ")
-            
-           
-            linha = int(input("Linha: "))
-            coluna = int(input("Coluna: "))
-
-            if nome_navio != "submarino":
-                orientacao_input = int(input("[1] Vertical [2] Horizontal >"))
-                orientacao = "vertical" if orientacao_input == 1 else "horizontal"
+            if nome == "submarino":
+                orientacao = "vertical"  # valor neutro para funções
             else:
-                orientacao = "vertical"
+                op = ler_int("[1] Vertical [2] Horizontal >")
+                orientacao = "vertical" if op == 1 else "horizontal"
 
-            if posicao_valida(frota, linha, coluna, orientacao, info["tamanho"]):
-                frota = preenche_frota(frota, nome_navio, linha, coluna, orientacao, info["tamanho"])
-                posicao_valida_flag = True
-            else:
-                print("Esta posição não está válida!", end=" ")
+            if not posicao_valida(frota, linha, coluna, orientacao, tamanho):
+                print("Esta posição não está válida!")
+                continue
 
-print(f"{frota}", end="")
+            preenche_frota(frota, nome, linha, coluna, orientacao, tamanho)
+            break
+
+print(frota)
