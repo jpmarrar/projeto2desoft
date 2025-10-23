9fb5a1b74e474692ef4962227d282ebb4a62ff7b
jpmarrar <Jpmarrar@uol.com.br>
2025-10-22 23:49:33 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,11 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         elif orientacao == "vertical":
             lista.append([linha + i, coluna])
     return lista
+    
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else:
+        frota[nome_navio] = [posicoes]
+    return frota
