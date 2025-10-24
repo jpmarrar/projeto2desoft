487aee598cab16d00804342cb67d2a7072a9ede4
jpmarrar <Jpmarrar@uol.com.br>
2025-10-24 10:33:50 -0300

    Improve input handling for ship placement
    Refactor ship information input and formatting.

--- programa.py
+++ programa.py
@@ -1,24 +1,27 @@
 from funcoes import define_posicoes, preenche_frota, posicao_valida
 
-frota = {"porta-aviões": [],"navio-tanque": [],"contratorpedeiro": [],"submarino": []}
+frota = {"porta-aviões": [], "navio-tanque": [], "contratorpedeiro": [], "submarino": []}
 
-navios_info = {"porta-aviões": {"tamanho": 4, "quantidade": 1},"navio-tanque": {"tamanho": 3, "quantidade": 2},"contratorpedeiro": {"tamanho": 2, "quantidade": 3},"submarino": {"tamanho": 1, "quantidade": 4}}
+navios_info = {
+    "porta-aviões": {"tamanho": 4, "quantidade": 1},
+    "navio-tanque": {"tamanho": 3, "quantidade": 2},
+    "contratorpedeiro": {"tamanho": 2, "quantidade": 3},
+    "submarino": {"tamanho": 1, "quantidade": 4}
+}
 
 for nome_navio, info in navios_info.items():
     for i in range(info["quantidade"]):
         posicao_valida_flag = False
-        while not posicao_valida_flag:
-            print(f"Insira as informações referentes ao navio {nome_navio} que possui tamanho {info['tamanho']}", end=" ")
 
+        print(f"Insira as informações referentes ao navio {nome_navio} que possui tamanho {info['tamanho']}", end=" ")
+
+        while not posicao_valida_flag:
             linha = int(input("Linha: "))
             coluna = int(input("Coluna: "))
 
             if nome_navio != "submarino":
                 orientacao_input = int(input("[1] Vertical [2] Horizontal >"))
-                if orientacao_input == 1:
-                    orientacao = "vertical"
-                else:
-                    orientacao = "horizontal"
+                orientacao = "vertical" if orientacao_input == 1 else "horizontal"
             else:
                 orientacao = "vertical"
 
@@ -27,5 +30,6 @@ for nome_navio, info in navios_info.items():
                 posicao_valida_flag = True
             else:
                 print("Esta posição não está válida!", end=" ")
+                print(f"Insira as informações referentes ao navio {nome_navio} que possui tamanho {info['tamanho']}", end=" ")
 
 print(f"{frota}", end="")
